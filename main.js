1079fae9d6700e13b81d34da9f9122b4f6072916
David Lau <David.lau@codeimmersives.com>
2020-04-13 16:34:28 -0400

    finished

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 const Stack = function() {
+  return {
+    items: [],
+    add: function(num){
+      this.items.push(num)
+    },
+    remove: function(){
+      return this.items.pop()
+    },
+    peek: function(){
+      return this.items[this.items.length-1]
+    }
+  }
 }
 
+
+
 const Queue = function() {
+  const que = Stack()
+  que.remove = function(){
+    return this.items.shift()
+  }
+  que.peek = function(){
+    return this.items[0]
+  }
+  return que
+  
 }
 
 module.exports = {
